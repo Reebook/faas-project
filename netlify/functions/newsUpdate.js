cdf1aed9f518bf0fa2c61236862453de59e86bd7
David Alvarez <hola@alvarezdavid.com>
2021-11-23 21:14:47 -0600

    First commit

---
+++ netlify/functions/newsUpdate.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const clientPromise = require('./mongoDB');
+const headers = require('./headersCORS');
+
+exports.handler = async (event, context) => {
+
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+
+  try {
+    const client = await clientPromise;
+    const id = event.path.split("/").reverse()[0];
+    const data = JSON.parse(event.body);
+
+    await client.db("tvnews").collection("news").updateOne({_id:String(id)},{$set:data});
+
+    return { statusCode: 200, headers, body: 'OK'};
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 422, headers, body: JSON.stringify(error) };
+  }
+};
