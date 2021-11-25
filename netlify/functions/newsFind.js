421a03e8b1c86fb3113350e915a0183c0748c19d
David Alvarez <hola@alvarezdavid.com>
2021-11-24 23:57:55 -0600

    Remove code

--- netlify/functions/newsFind.js
+++ netlify/functions/newsFind.js
@@ -5,10 +5,6 @@ const headers = require('./headersCORS');
 
 exports.handler = async (event, context) => {
 
-  if (event.httpMethod == "OPTIONS") {
-    return { statusCode: 200, headers, body: "OK" };
-  }
-
   try {
     const client = await clientPromise;
     const id = event.path.split("/").reverse()[0];
--- netlify/functions/newsFindAll.js
+++ netlify/functions/newsFindAll.js
@@ -4,11 +4,7 @@ const clientPromise = require('./mongoDB');
 const headers = require('./headersCORS');
 
 exports.handler = async (event, context) => {
-
-  if (event.httpMethod == "OPTIONS") {
-    return { statusCode: 200, headers, body: "OK" };
-  }
-
+  
   try {
     const client = await clientPromise;
     const authors = await client.db("tvnews").collection("news").find({}).toArray();
