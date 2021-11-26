4abf50a241d0ce591a1d376fb1ea461298fbc16a
David Alvarez <hola@alvarezdavid.com>
2021-11-26 17:50:58 -0600

    Console log delete

--- netlify/functions/newsDelete.js
+++ netlify/functions/newsDelete.js
@@ -8,7 +8,7 @@ exports.handler = async (event, context) => {
   try {
     const client = await clientPromise;
     const id = event.path.split("/").reverse()[0];
-
+    console.log(id)
     await client.db("tvnews").collection("news").deleteOne({_id:id});
 
     return { statusCode: 200, headers, body: 'OK'};
