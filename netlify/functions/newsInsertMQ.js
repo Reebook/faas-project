74548cc9fd3e67b8934c51031a24d4e051c7275c
David Alvarez <hola@alvarezdavid.com>
2021-11-26 00:55:25 -0600

    Update MQ insert

--- netlify/functions/newsInsertMQ.js
+++ netlify/functions/newsInsertMQ.js
@@ -7,10 +7,8 @@ const rabbitPromise = require('./rabbitMQ');
 exports.handler = async (event, context) => {
 
   try {
-    const id = event.path.split("/").reverse()[0];
-
     const channel = await rabbitPromise();
-    const request = `{'method':'INSERT','id':${id},'body':${event.body}}`;
+    const request = `{'method':'INSERT','body':${event.body}}`;
     await channel.sendToQueue("newsStore", Buffer.from(request));
 
     return { statusCode: 200, headers, body: 'OK'};
