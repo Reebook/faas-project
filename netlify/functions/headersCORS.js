02cbe6dec80cfce92a5e9d5a6b33df2bf09b3149
David Alvarez <hola@alvarezdavid.com>
2021-11-26 01:32:08 -0600

    update CORS

--- netlify/functions/headersCORS.js
+++ netlify/functions/headersCORS.js
@@ -2,12 +2,11 @@
 
   // To enable CORS
 
-const headers =  {
+module.exports =  {
   'Accept': 'application/json',
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Credentials': true,
-  'Access-Control-Allow-Headers': 'Authorization, X-Requested-With, Access-Control-Allow-Headers, Origin, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers',
+  //'Access-Control-Allow-Headers': 'Authorization, X-Requested-With, Access-Control-Allow-Headers, Origin, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers',
+  "Access-Control-Allow-Headers": "Content-Type",
   'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE'
 };
-
-export default headers
