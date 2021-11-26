b3562573510db791ac3f04e3cbbf18e72287f259
David Alvarez <hola@alvarezdavid.com>
2021-11-26 01:45:21 -0600

    Update CORS

--- netlify/functions/headersCORS.js
+++ netlify/functions/headersCORS.js
@@ -6,7 +6,7 @@ module.exports =  {
   'Accept': 'application/json',
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Credentials': true,
-  //'Access-Control-Allow-Headers': 'Authorization, X-Requested-With, Access-Control-Allow-Headers, Origin, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers',
-  "Access-Control-Allow-Headers": "Content-Type",
+  'Access-Control-Allow-Headers': 'Authorization, X-Requested-With, Access-Control-Allow-Headers, Origin, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers',
+  //"Access-Control-Allow-Headers": "Content-Type",
   'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE'
 };
