f9fa6308a21221fb30dca32f536eaf43df2921c8
David Alvarez <hola@alvarezdavid.com>
2021-11-26 00:43:39 -0600

    Change headers

--- netlify/functions/headersCORS.js
+++ netlify/functions/headersCORS.js
@@ -6,6 +6,6 @@ module.exports =  {
   'Accept': 'application/json',
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Credentials': true,
-  'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers',
+  'Access-Control-Allow-Headers': 'Authorization, X-Requested-With, Access-Control-Allow-Headers, Origin, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers',
   'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE'
 };
